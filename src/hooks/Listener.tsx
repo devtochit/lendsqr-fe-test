45f72a833412faeb6b33750d7da8516b5aa32770
Layoolar <46034532+Layoolar@users.noreply.github.com>
2023-01-07 01:56:31 +0100

    Implemented lifecycle hooks

---
+++ src/hooks/Listener.tsx
@@ -0,0 +1,26 @@
+import { useEffect, RefObject } from 'react';
+
+type Event = MouseEvent | TouchEvent;
+
+const Listener = <T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  handler: (event: Event) => void
+) => {
+  useEffect(() => {
+    const listener = (event: Event) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        handler(event);
+      }
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+};
+
+export default Listener;
