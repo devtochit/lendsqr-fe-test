ad03098010519d093dae305bab94487297dddfab
Layoolar <46034532+Layoolar@users.noreply.github.com>
2023-01-07 03:49:28 +0100

    Implemented the user component

--- src/constants/icons/UserSavingsIcon.tsx
+++ src/constants/icons/UserSavingsIcon.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const UserSavings = () => {
+const UserSavingsIcon = () => {
     return (
         <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle opacity="0.1" cx="20" cy="20" r="20" fill="#FF3366" />
@@ -26,4 +26,4 @@ const UserSavings = () => {
     )
 }
 
-export default UserSavings
+export default UserSavingsIcon
--- src/screens/Users.tsx
+++ src/screens/Users.tsx
@@ -1,1 +1,70 @@
-import { lazy } from 'react'
+import { lazy, useEffect } from 'react'
+import ActiveUsersIcon from '../constants/icons/ActivateUserIcon';
+import UserLoansIcon from '../constants/icons/UserLoansIcon';
+import UserSavingsIcon from '../constants/icons/UserSavingsIcon';
+import UsersIcon from '../constants/icons/UsersIcon';
+import { useAppDispatch, useAppSelector } from '../hooks/hooks';
+import '../saas/Users/users.scss'
+import { getAllUsers } from '../functions/usersSlice';
+import { getYearsBetween } from '../constants/utils/helpers';
+
+import React from 'react'
+
+export default function Users() {
+    const dispatch = useAppDispatch()
+  const { filteredUsers, isError, isLoading, message } = useAppSelector((state: any) => state.users)
+
+    const activeUsers = filteredUsers.filter((val: any) => getYearsBetween(val.createdAt, val.lastActiveDate) < 40)
+    const usersWithLoan = filteredUsers.filter((val:any) => (val?.education?.loanRepayment) > 0 )
+    const getAllEarners = filteredUsers.map((val:any) => (val?.education?.monthlyIncome) )
+    const xt = getAllEarners.map((val:any) => (Math.trunc(Number(val[1])) - Math.trunc(Number(val[0]))))
+    const getUsersWithSaving = xt.filter((val:any) => val > -1);
+    
+    useEffect(() => {
+    dispatch(getAllUsers())
+  }, [])
+
+    const cardInfo: { title: string; data: string; icon: any }[] = [
+    {
+      title: 'users',
+      icon: <UsersIcon />,
+      data: filteredUsers?.length || 0
+    },
+    {
+      title: 'active users',
+      icon: <ActiveUsersIcon />,
+      data: activeUsers.length || 0
+    },
+    {
+      title: 'users with loan',
+      icon: <UserLoansIcon />,
+      data: usersWithLoan.length || 0
+    },
+    {
+      title: 'users with savings',
+      icon: <UserSavings />,
+      data: getUsersWithSaving.length || 0
+    },
+  ]
+
+  const renderCards = cardInfo.map((card, i) => (
+    <UserCards  key={i} i={i} card={card} />
+  ))
+
+
+  return (
+    <div>
+      {isLoading && <Loader />}
+      {isError && <span>{message}</span>}
+      {!isError &&
+        <div className='users__wrapper'>
+        <h1 className='heading'>Users</h1>
+        <div className='cards__wrapper'>
+          {renderCards}
+        </div>
+          {!isLoading && <UsersTable tableData={filteredUsers}/> }
+      </div>
+      }
+    </div>
+  )
+}
