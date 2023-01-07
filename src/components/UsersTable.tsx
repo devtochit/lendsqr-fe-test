786531897f8434e2b4c4635e6d1d1fcbbb4e0439
Layoolar <46034532+Layoolar@users.noreply.github.com>
2023-01-07 14:48:23 +0100

    Implemented the user table component

--- src/components/UsersTable.tsx
+++ src/components/UsersTable.tsx
@@ -1,14 +1,144 @@
-import ActivateUserIcon from "../constants/icons/ActivateUserIcon"
-import BlackListUserIcon from "../constants/icons/BlackListUserIcon"
-import Ellipsis from "../constants/icons/Ellipsis"
-import EyeIcon from "../constants/icons/EyeIcon"
-import SortIcon from "../constants/icons/SortIcon"
-import Filters from "./Filters"
-import { headings } from "../constants/utils/data"
+import ActivateUserIcon from "../constants/icons/ActivateUserIcon";
+import BlackListUserIcon from "../constants/icons/BlackListUserIcon";
+import Ellipsis from "../constants/icons/Ellipsis";
+import EyeIcon from "../constants/icons/EyeIcon";
+import SortIcon from "../constants/icons/SortIcon";
+import Filters from "./Filters";
+import { headings } from "../constants/utils/data";
 import { useState, useRef } from 'react';
 import { characterLimit, formatDate, getYearsBetween  } from "../constants/utils/helpers"
 import Listener from '../hooks/Listener';
 import { motion } from 'framer-motion';
 import CaretDownIcon from "../constants/icons/CaretDownIcon";
+import Pagination from "./constants/Pagination";
+import '../saas/Users/users.scss';
+
+interface Props {
+    tableData: [],
+}
+
+
+const UsersTable = ({ tableData }: Props) => {
+    const [activeIndex, setActiveIndex] = useState<any>('')
+    const [isFilterModal, setIsFilterModal] = useState<boolean>(false)
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [usersPerPage, setUsersPerPage] = useState<number>(10)
+    const [selected, setSelcted] = useState<number>(10)
+    const [showDrop, setShowDrop] = useState<boolean>(false)
+    const indexOfLastUser = currentPage * usersPerPage;
+    const indexOfFirstUser = indexOfLastUser - usersPerPage;
+    const currentUsers = tableData && tableData.slice(indexOfFirstUser, indexOfLastUser)
+    const promptModalRef = useRef<HTMLDivElement>(null);
+    const clickOutsidehandler = () => { setActiveIndex('') };
+    Listener(promptModalRef, clickOutsidehandler);
+
+
+
+    const renderTableHeadings = headings.map((val: any, i: number) => (
+        <th
+            onClick={() => setIsFilterModal(!isFilterModal)} key={i}>
+            <span className='table__headings'>{val.title}&nbsp;
+                <span style={{ cursor: 'pointer' }}><SortIcon /></span>
+            </span>
+        </th>
+    ))
+
+
+    const renderUsers = currentUsers?.map((user: any, i: number) => (
+        <motion.tr key={i}
+        >
+            <td className='row__data'>{characterLimit(user.orgName, 24)} </td>
+            <td>{user.userName}</td>
+            <td>{characterLimit(user.email, 20)}</td>
+            <td>{user.phoneNumber}</td>
+            <td>{formatDate(user.createdAt)}</td>
+            <td>{
+                (getYearsBetween(user.createdAt, user.lastActiveDate)) <= 40 ? <span className="status__btn active-status">{'Active'}</span>
+                    : (getYearsBetween(user.createdAt, user.lastActiveDate)) > 40 && (getYearsBetween(user.createdAt, user.lastActiveDate)) <= 60 ? <span className="status__btn pending-status">{'Pending'}</span>
+                        : (getYearsBetween(user.createdAt, user.lastActiveDate)) > 60 && (getYearsBetween(user.createdAt, user.lastActiveDate)) <= 90 ? <span className="status__btn inactive-status">{'Inactive'}</span>
+                            : (getYearsBetween(user.createdAt, user.lastActiveDate)) > 90 && <span className="status__btn blacklisted-status">{'Blacklisted'}</span>
+            }
+            </td>
+            <td onClick={() => setActiveIndex(i)} className='action__group'><Ellipsis />
+                {activeIndex === i &&
+                    <motion.div ref={promptModalRef}
+                        initial={{ opacity: 0, translateX: -50, zIndex: 99 }}
+                        animate={{ opacity: 1, translateX: 0, zIndex: 99 }}
+                        transition={{ duration: 0.4, ease: [0.43, 0.13, 0.23, 0.96], delay: i * 0.1 }}
+                        className='action__prompt'>
+                        <a href={`users/${user.id}`}><div><EyeIcon /> View Details</div></a>
+                        <div><BlackListUserIcon /> Blacklist User</div>
+                        <div><ActivateUserIcon /> Activate User</div>
+                    </motion.div>
+                }
+            </td>
+        </motion.tr>
+    ))
+
+
+// muber per-page control
+    const arr = Array.from({ length: 5 }, (_, i) => (i + 1) * 5)
+    const arrOfStr = arr.map(num => { return String(num); });
+    function autoSetUserPerPage(item: any, index: number) {
+        var newOptions = { option: item };
+        return newOptions;
+    }
+    var options = arrOfStr.map(autoSetUserPerPage);
+
+
+    const updateVal = (val: any) => {
+        setSelcted(val.option)
+        setUsersPerPage(val.option)
+        setShowDrop(!showDrop)
+    }
+
+    return (
+        <>
+            {isFilterModal && <Filters currentUsers={currentUsers} setIsFilterModal={setIsFilterModal} />}
+            <section className='table__container'>
+                <table className="table__wrapper">
+                    <thead>
+                        <tr>
+                            {renderTableHeadings}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {renderUsers}
+                    </tbody>
+                </table>
+            </section>
+            <div className='table__footer'>
+                <section className='page__number'>
+                    <p>Showing</p>
+                    <div className='dropbox'>
+                        <div onClick={() => setShowDrop(!showDrop)} className='selected'>
+                            <div>{tableData.length > selected ? selected : tableData.length}</div>
+                            <p><CaretDownIcon /></p>
+                        </div>
+                        {
+                            showDrop &&
+                            <div className='options'>
+                                {options?.map((val: any, i: number) =>
+                                    <p key={i} onClick={() => updateVal(val)}>{val.option}</p>
+                                )}
+                            </div>
+                        }
+                    </div>
+                    <p>out of {tableData.length}</p>
+                </section>
+                <div>
+                    <Pagination
+                        currentPage={currentPage}
+                        lastPage={tableData.length / usersPerPage}
+                        maxLength={7}
+                        setCurrentPage={setCurrentPage}
+                    />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default UsersTable
 
 
--- src/components/constants/Pagination.tsx
+++ src/components/constants/Pagination.tsx
@@ -1,5 +1,52 @@
 import NextBtn from "../../constants/icons/NextBtn"
 import PrevBtn from "../../constants/icons/PrevBtn"
 import { getPaginationItems } from "../../functions/pagination"
+import PageLink from "./PageLink";
+import '../../saas/Pagination/pagination.scss'
 
 
+export type Props = {
+  currentPage: number;
+  lastPage: number;
+  maxLength: any;
+  setCurrentPage: (page: number) => void;
+};
+
+export default function Pagination({
+  currentPage,
+  lastPage,
+  maxLength,
+  setCurrentPage,
+}: Props) {
+  const pageNums = getPaginationItems(currentPage, lastPage, maxLength);
+  
+
+  return (
+    <>
+    <nav className="pagination" aria-label="Pagination">
+      <PageLink
+        disabled={currentPage === 1}
+        onClick={() => setCurrentPage(currentPage - 1)}
+      >
+        <PrevBtn />
+      </PageLink>
+      {pageNums.map((pageNum, idx) => (
+        <PageLink
+          key={idx}
+          active={currentPage === pageNum}
+          disabled={isNaN(pageNum)}
+          onClick={() => setCurrentPage(pageNum)}
+        >
+          {!isNaN(pageNum) ? pageNum : '...'}
+        </PageLink>
+      ))}
+      <PageLink
+        disabled={currentPage === lastPage}
+        onClick={() => setCurrentPage(currentPage + 1)}
+      >
+        <NextBtn />
+      </PageLink>
+    </nav>
+    </>
+  );
+} 
